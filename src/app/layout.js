77011ae8bb724fe681f3576e47a22d4c9135cfe6
Oskar Duveborn <weyland@flowersfx.com>
2023-10-18 23:34:30 +0200

    initial video test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+const freePixel = localFont({ src: './freepixel.woff2', variable: '--font-freepixel', display: 'swap' })
 
 export const metadata = {
   title: 'Syntax Error Stockholm Night Club',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={freePixel.className}>{children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
 import Image from 'next/image'
+import localFont from 'next/font/local'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <video autoPlay muted loop playsInline className="object-cover w-full h-full fixed -z-10">         
+          <source src="/video.mp4" type="video/mp4"/>       
+      </video>      
+      <section className='p-6 min-h-screen'>
+        <h1>Syntax Error Halloween Special</h1>
+        <p>It's finally Halloween time! All the artists, all the video games and all the cosplay you'd expect is right here! </p>
+        <div className='flex justify-center'>
+          <button>Book now!</button>
+          <button className='more'>Find out more</button>
+        </div>
+      </section>
+      <section>
+        <h2>Here is more</h2>
+        <p>bla bla</p>
+      </section>
     </main>
   )
 }
