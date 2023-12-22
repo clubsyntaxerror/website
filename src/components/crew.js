794edf21d8fb8bea380837d38ff37b27c8e3ba31
Oskar Duveborn <oskar@duveborn.se>
2023-12-22 19:07:06 +0100

    new board

--- src/components/crew.js
+++ src/components/crew.js
@@ -15,7 +15,7 @@ export default function Crew() {
         
             <TabPanel>
             <h2>Active crew members who also DJ</h2>
-            <p>Hakushi, Ventura, Velo, Fastbom, Weyland, Kim, MissStabby, Jor-el</p>
+            <p>Ventura, Fastbom, Weyland, Kim, MissStabby, Jor-el</p>
             </TabPanel>
             <TabPanel>
             <h2>Active non-DJ crew members</h2>
@@ -23,11 +23,11 @@ export default function Crew() {
             </TabPanel>
             <TabPanel>
                 <h2>The current board of the non-profit organisation Ideella Spelmusikfrämjandet</h2>
-                <p>Hakushi (chairman), Weyland (treasurer), Matti, MissStabby, Jor-el</p>
+                <p>Ventura (chairman), Weyland (treasurer), Matti, MissStabby, Fastbom</p>
             </TabPanel>
             <TabPanel>
                 <h2>Inactive crew members</h2>
-                <p>Jon, Matti, Lindroth, Wezz, Daniel, Nik, Whiting, Borchers, McFly, Windefalk, MrTimpi</p>
+                <p>Jon, Matti, Lindroth, Wezz, Daniel, Nik, Whiting, Borchers, McFly, Windefalk, MrTimpi, Hakushi, Velo</p>
             </TabPanel>        
         </Tabs>
    )
