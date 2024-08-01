7b3382c84e17c9b2dddd45acb8b8eee0b019e589
Oskar Duveborn <oskar@duveborn.se>
2024-08-01 18:17:24 +0200

    microcopy deletion

--- src/components/hero.js
+++ src/components/hero.js
@@ -18,34 +18,18 @@ export default function Hero({featuredEvent}) {
                         <h1 className='rainbow_text_animated'>{featuredEvent.eventName}</h1>
                         <p className='text-xl md:text-3xl'>{featuredEvent.eventDescription}</p>
                         <p className='mb-0 md:text-xl'>{featuredEvent.longDate} at {featuredEvent.venueName}</p>
-                        <p className='mb-0 md:text-xl'>{featuredEvent.openingHours}{featuredEvent.optionalCoverFee ? ', admission ' + featuredEvent.optionalCoverFee + ' SEK' : ''} </p>
+                        <p className='mb-0 md:text-xl'>{featuredEvent.openingHours}{featuredEvent.optionalCoverFee ? ', admission ' + featuredEvent.optionalCoverFee + ' SEK' : ', free to attend'} </p>
                     </div>
                     </div>
                     <div className='flex justify-center'>
                     {featuredEvent.optionalCallToActionTitle && featuredEvent.optionalCallToActionUrl && (
                         <>
                         <Link href={featuredEvent.optionalCallToActionUrl} target="_blank" className='button bg-white text-black'>{featuredEvent.optionalCallToActionTitle}</Link>
-                        <Link href='#events' className='button more bg-black'>More events</Link>
+                        <Link href='#events' className='button more bg-black'>Show more events</Link>
                         </>
                     )}
                     {(!featuredEvent.optionalCallToActionTitle || !featuredEvent.optionalCallToActionUrl) && (
-                        <>
-                        <Popup
-                            trigger={open => (
-                                <button className="button bg-white text-black">{open ? 'OK' : 'No presale'}</button>
-                            )}
-                            position="top left"
-                            closeOnDocumentClick
-                            >
-                                {featuredEvent.optionalCoverFee && (
-                                    <div className='text-black'><p>Tickets to the next event are currently only sold at the door.</p><p>We accept cards, Swish and cash.</p></div>
-                                )}
-                                {!featuredEvent.optionalCoverFee && (
-                                    <div className='text-black'><p>This event is free to attend.</p></div>
-                                )}
-                        </Popup>                   
-                        <Link href='#events' className='button more bg-black'>More events</Link>
-                        </>
+                        <Link href='#events' className='button more bg-black'>Show more events</Link>
                     )}              
                     </div>
                 </>
