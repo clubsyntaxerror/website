97339a88efcf4953e3fc96d6fd30cdf293d17f49
Astrid Gealer <astrid@gealer.email>
2025-01-12 19:08:05 +0000

    make this null

--- website/src/getEvents.ts
+++ website/src/getEvents.ts
@@ -1,16 +1,20 @@
 import { db, schema } from "database";
 import { cache } from "react";
-import { gte, sql, asc } from "drizzle-orm";
+import { gte, sql, asc, eq } from "drizzle-orm";
 
 export const getEvents = cache(async () => {
-    return db.query.events.findMany({
-        where: gte(schema.events.eventStart, sql`now()`),
-        orderBy: asc(schema.events.eventStart),
-        limit: 7,
-        with: {
-            callToAction: true,
-        },
-    });
+    const events = await db
+        .select()
+        .from(schema.events)
+        .where(gte(schema.events.eventStart, sql`now()`))
+        .orderBy(asc(schema.events.eventStart))
+        .limit(7)
+        .leftJoin(schema.callToAction, eq(schema.events.callToActionId, schema.callToAction.id));
+
+    return events.map((e) => ({
+        ...e.events,
+        callToAction: e.call_to_action,
+    }));
 });
 
 export type Event = Awaited<ReturnType<typeof getEvents>>[number];
