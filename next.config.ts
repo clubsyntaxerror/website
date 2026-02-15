5ec0522e5773414559239da68a1cf1b4910c6bce
Oskar Duveborn <oskar.duveborn@valtech.com>
2026-02-15 12:46:08 +0100

    added privacy policy for the crewapp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/privacy-policy-crewapp",
+                destination: "/privacy-policy-crewapp.html",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
